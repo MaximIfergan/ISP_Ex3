04c59d2d922ebca84fb8f7d4733412393153fc7c
maxim <maxim758@gmail.com>
2024-07-21 16:56:47 +0300

    Fixing some bugs

--- ctc.py
+++ ctc.py
@@ -17,22 +17,26 @@ def ctc_loss(y, p, alphabet):
     # Initialize alpha:
     alpha = np.zeros((S, T))
     alpha[0, 0] = y[0, 0]
-    alpha[1, 0] = y[0, alphabet.index(z[1])]  # Maybe z1 - > z0
+    alpha[1, 0] = y[0, alphabet.index(z[1]) + 1]
     alpha[2:, 0] = 0
 
     # Dynamic Programming:
     for t in range(1, T):
         for s in range(S):
+            y_col = 0 if z[s] == "" else alphabet.index(z[s]) + 1
             if s == 0:
-                alpha[s, t] = alpha[s, t - 1] * y[t, 0]
+                alpha[s, t] = alpha[s, t - 1] * y[t, y_col]
             elif s == 1: # z[s] != "" (epsilon)
-                alpha[s, t] = (alpha[s - 1, t - 1] + alpha[s, t - 1]) * y[t, alphabet.index(z[s])]
+                alpha[s, t] = (alpha[s - 1, t - 1] + alpha[s, t - 1]) * y[t, y_col]
             else:
                 if z[s] == '' or z[s] == z[s - 2]:
-                    alpha[s, t] = (alpha[s - 1, t - 1] + alpha[s, t - 1]) * y[t, alphabet.index(z[s])]
+                    alpha[s, t] = (alpha[s - 1, t - 1] + alpha[s, t - 1]) * y[t, y_col]
                 else:
                     alpha[s, t] = (alpha[s - 2, t - 1] + alpha[s - 1, t - 1] + alpha[s, t - 1]) * y[
-                        t, alphabet.index(z[s])]
+                        t, y_col]
+
+    print("=== alpha ===")
+    print(alpha)
 
     return alpha[-1, -1]
 
@@ -52,6 +56,9 @@ if __name__ == "__main__":
 
     # Load the matrix
     y = np.load(matrix_path)
+
+    print("=== y ===")
+    print(y)
 
     # Calculate P(p|y)
     p = ctc_loss(y, labeling, alphabet)
